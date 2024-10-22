b64cc05bb8d97c9b2d2efef223c389b39c96ba88
Yuri <wall.mart.boss@proton.me>
2024-10-23 01:16:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <BrowserRouter basename="/Wallmartboss/Last_Project_ReactNode">
     <App />
+   <BrowserRouter />
   </React.StrictMode>
 );
